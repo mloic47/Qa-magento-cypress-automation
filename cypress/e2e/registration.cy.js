872a30a3e4434c3ee279db2546ef81916b460437
Le Roy Loic Mewoulou <loic.mewoulou@cm.maviance.com>
2025-04-23 17:12:59 +0100

    fixed all issues relating to registration.cy.js and it validations

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,14 +1,13 @@
-import RegistrationPage from '../../pages/RegistrationPage';
-import LoginPage from '../../pages/LoginPage';
-import { ROUTES } from '../../constants/routes';
+import RegistrationPage from '../pages/RegistrationPage';
+import LoginPage from '../pages/LoginPage';
+import { ROUTES } from '../constants/routes';
 
 describe('Registration Flow with Login Validation', () => {
     const registrationPage = new RegistrationPage();
     const loginPage = new LoginPage();
 
     it('should register a user and validate login', () => {
-        cy.fixture('userData.json').then((userData) => {
-            const user = userData.validUser;
+            const user = Cypress.env('validUser');
 
             cy.visit(ROUTES.REGISTER);
             registrationPage.register(user);
@@ -17,7 +16,7 @@ describe('Registration Flow with Login Validation', () => {
             loginPage.login(user);
 
             cy.url().should('include', ROUTES.ACCOUNT_DASHBOARD);
-            cy.get('.greet.welcome').should('contain', `Hello, ${user.firstName} ${user.lastName}`);
-        });
+            cy.contains( `${user.firstName} ${user.lastName}`);
+
     });
 });
